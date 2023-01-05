7cd026103e0e26cb1dbf6e89423a5f645965affb
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-05 17:49:37 +0000

    Add incomplete pool puzzle

---
+++ Chapter 3/Triangle.java
@@ -0,0 +1,36 @@
+// Fill in the lines of code to get the following output:
+// triangle 0, area = 4.0
+// triangle 1, area = 10.0
+// triangle 2, area = 18.0
+// triangle 3, area = ____
+// y = ___________________
+
+class Triangle {
+  double area;
+  int height;
+  int length;
+  
+  public static void main(String[] args) {
+  _____________
+  _______________________________
+  while (____________) {
+    ___________________________
+    ________.height = (x + 1) * 2;
+    ________.height = x + 4;
+    ________________________
+    System.out.print("triangle" + x + ", area");
+    System.out.println("=" + _____________.area);
+    ____________________
+    }
+  _____________
+  x = 27
+  Triangle t5 = ta[2];
+  ta[2].area = 343;
+  System.out.print("y = " + y);
+  System.out.println(", t5 area = " + t5.area);
+  }
+  void setArea() {
+  __________ = (height * length) / 2;
+  
+  }
+}
