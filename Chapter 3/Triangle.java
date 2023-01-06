5ad65c79283ea3e4834b7bfe82307ab3b2b89eb3
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-06 10:13:42 +0000

    Complete Triangle pool exercise

--- Chapter 3/Triangle.java
+++ Chapter 3/Triangle.java
@@ -1,9 +1,9 @@
-// Fill in the lines of code to get the following output:
+// Fill in the lines of code with the given snippets to get the following output:
 // triangle 0, area = 4.0
 // triangle 1, area = 10.0
 // triangle 2, area = 18.0
-// triangle 3, area = ____
-// y = ___________________
+// triangle 3, area = 28
+// y = 4, t5 area = 343
 
 class Triangle {
   double area;
@@ -11,26 +11,26 @@ class Triangle {
   int length;
   
   public static void main(String[] args) {
-  _____________
-  _______________________________
-  while (____________) {
-    ___________________________
-    ________.height = (x + 1) * 2;
-    ________.height = x + 4;
-    ________________________
-    System.out.print("triangle" + x + ", area");
-    System.out.println("=" + _____________.area);
-    ____________________
+  int x = 0;
+  Triangle [] ta = new Triangle[4];
+  while (x < 4) {
+    ta[x] = new Triangle();
+    ta[x].height = (x + 1) * 2;
+    ta[x].length = x + 4;
+    ta[x].setArea();
+    System.out.print("triangle " + x + ", area");
+    System.out.println(" = " + ta[x].area);
+    x = x + 1;
     }
-  _____________
-  x = 27
+  int y = x;
+  x = 27;
   Triangle t5 = ta[2];
   ta[2].area = 343;
   System.out.print("y = " + y);
   System.out.println(", t5 area = " + t5.area);
   }
   void setArea() {
-  __________ = (height * length) / 2;
+  area = (height * length) / 2;
   
   }
 }
