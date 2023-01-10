e6945dc1bf0848cf61ca5b1ec9a8acb9c1a2f0cf
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-10 13:43:14 +0000

    Remove some comments

--- Chapter 5/SimpleStartupGame.java
+++ Chapter 5/SimpleStartupGame.java
@@ -1,25 +1,22 @@
 class SimpleStartupGame {
   public static void main (String [] args) {
     int numOfGuesses;      
-    GameHelper helper = new GameHelper // This is a special classs we wrote that has the method for getting user input. For now, pretend it's part of Java
+    GameHelper helper = new GameHelper 
     
-   SimpleStartup theStartup = new SimpleStartup(); // MAKE a new SimpleStartup instance                
+   SimpleStartup theStartup = new SimpleStartup();              
    int randomNum = (int) (Math.random() * 5);
     
    int[] locations = (randomNum, randomNum + 1, randomNum + 2); // MAKE an int array with 3 ints using the randomly generated number, that number incremented by 1, and that number incremented by 2 (example 3, 4, 5)
-   theStartup.setLocationCells(locations); // INVOKE the setLocationCells() method on the SimpleStartup instance (Give the startup its locations)
-   boolean isAlive = true;  // DECLARE a boolean variable representing the state of the game, named isAlive. SET it to true
+   theStartup.setLocationCells(locations);
    
-   while (isAlive) {      // WHILE the Startup is alive (isAlive == true) 
+   while (isAlive) {     
      int guess = helper.getUserInput("enter a number");        // GET user input from the command line
      String result = theStartup.checkYourself(guess);
-     numOfGuesses++;     // INCREMENT numOfGuesses variable
+     numOfGuesses++;     
      if (result.equals("kill")) {
        isAlive = false;
-       System.out.pringln("You took " + numOfGuesses + " guesses"); // // CHECK for Startup dead
-     // IF result is "kill"
-     //    SET isAlive to false
-     //    PRINT number of guesses
+       System.out.pringln("You took " + numOfGuesses + " guesses"); 
+      } // End of if
    }
   } // End of method
 } // End of class
