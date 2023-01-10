e425454a7481729a8ed560b4a9fae0cfd3aebff0
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-10 10:34:45 +0000

    Write pseudocode for SimpleStartup game class

---
+++ Chapter 5/SimpleStartupGame.java
@@ -0,0 +1,7 @@
+class SimpleStartupGame {
+  // METHOD public static void main (String [] args)
+  // DECLARE an int variable to hold the number of user guesses, named numOfGuesses
+  // COMPUTE a random number between 0 and 4 that will be the starting location cell position
+  // WHILE the Startup is still alive:
+  // GET user input from the command line
+} // End of class
