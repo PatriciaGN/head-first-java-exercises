9292ad38d4d9b361fde03d3d4546906da342f7bc
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-10 11:35:35 +0000

    Add basic code forSimpleStartupGame class

--- Chapter 5/SimpleStartupGame.java
+++ Chapter 5/SimpleStartupGame.java
@@ -1,7 +1,13 @@
 class SimpleStartupGame {
-  // METHOD public static void main (String [] args)
-  // DECLARE an int variable to hold the number of user guesses, named numOfGuesses
-  // COMPUTE a random number between 0 and 4 that will be the starting location cell position
-  // WHILE the Startup is still alive:
-  // GET user input from the command line
+  public static void main (String [] args) {
+    int numOfGuesses;                      // DECLARE an int variable to hold the number of user guesses, named numOfGuesses
+   java.util.Random randomGenerator = new java.util.Random(); 
+   int startingPosition = randomGenerator.nextint(5); // COMPUTE a   random number between 0 and 4 that will be the starting location cell position
+   while (numOfHits < 3) {      // WHILE the Startup is still alive:
+     Scanner userInput = new Scanner(System.in) // Create scanner object
+     System.out.println("enter a number");
+     String guess = userInput.nextLiner();   // GET user input from the command line
+     System.out.println(result);
+   }
+  } // End of method
 } // End of class
