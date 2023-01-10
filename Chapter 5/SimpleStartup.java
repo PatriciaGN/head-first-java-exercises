1faceaf18af279e1877c9c7b3dc5a75667a5158c
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-10 09:54:30 +0000

    Add pseudocode for checkYourself method

---
+++ Chapter 5/SimpleStartup.java
@@ -0,0 +1,15 @@
+public class SimpleStartup {
+  // METHOD String checkYourself(int userGuess)
+  //   GET the user guess as an int parameter
+  //   REPEAT with each of the location cells in the int array
+  //     // COMPARE the user guess to the location cell
+  //     IF the user guess matches
+  //       INCREMENT the number of hits
+  //       // FIND OUT if it was the last location cell:
+  //       IF number of hits is 3, RETURN "Kill" as the result
+  //       ELSE it was not a kill, so RETURN "Hit"
+  //       END IF
+  //     ELSE the user guess did not match, so RETURN "Miss"
+  //     END IF
+  //   END REPEAT
+  // END METHOD
