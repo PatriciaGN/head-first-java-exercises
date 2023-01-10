9df424904e779fbe95288a3a02f7460d3305cd4a
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-10 10:06:00 +0000

    Write code for checkYourself() method

--- Chapter 5/SimpleStartup.java
+++ Chapter 5/SimpleStartup.java
@@ -1,15 +1,17 @@
 public class SimpleStartup {
-  // METHOD String checkYourself(int userGuess)
-  //   GET the user guess as an int parameter
-  //   REPEAT with each of the location cells in the int array
-  //     // COMPARE the user guess to the location cell
-  //     IF the user guess matches
-  //       INCREMENT the number of hits
-  //       // FIND OUT if it was the last location cell:
-  //       IF number of hits is 3, RETURN "Kill" as the result
-  //       ELSE it was not a kill, so RETURN "Hit"
-  //       END IF
-  //     ELSE the user guess did not match, so RETURN "Miss"
-  //     END IF
-  //   END REPEAT
-  // END METHOD
+  public String checkyourself(int guess) { // METHOD String checkYourself(int userGuess)
+    String result = "miss";                //   GET the user guess as an int parameter
+    for (int cell : locationCells) {       //   REPEAT with each of the location cells in the int array
+      if (guess == cell) {                 //     // COMPARE the user guess to the location cell
+        result = "hit";                    //     IF the user guess matches
+        numOfHits++;                       //       INCREMENT the number of hits
+        break;                             //       // FIND OUT if it was the last location cell:
+      }
+    }
+    if (numOfHits == locationCells.length) { //       IF number of hits is 3, RETURN "Kill" as the result
+      result = "kill";
+    }                                        //       ELSE it was not a kill, so RETURN "Hit"
+    System.out.println(result);
+    return result;
+  }
+}
