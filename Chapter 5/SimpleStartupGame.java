1a59499cb10067503cc91e9036e53855c779770d
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-10 12:01:18 +0000

    Complete code in main method for game class

--- Chapter 5/SimpleStartupGame.java
+++ Chapter 5/SimpleStartupGame.java
@@ -1,23 +1,25 @@
 class SimpleStartupGame {
   public static void main (String [] args) {
     int numOfGuesses;      
-    // MAKE a new SimpleStartup instance                
-   java.util.Random randomGenerator = new java.util.Random(); 
-   int startingPosition = randomGenerator.nextint(5); 
-   // MAKE an int array with 3 ints using the randomly generated number, that number incremented by 1, and that number incremented by 2 (example 3, 4, 5)
-   // INVOKE the setLocationCells() method on the SimpleStartup instance
-   // DECLARE a boolean variable representing the state of the game, named isAlive. SET it to true
+    GameHelper helper = new GameHelper // This is a special classs we wrote that has the method for getting user input. For now, pretend it's part of Java
+    
+   SimpleStartup theStartup = new SimpleStartup(); // MAKE a new SimpleStartup instance                
+   int randomNum = (int) (Math.random() * 5);
+    
+   int[] locations = (randomNum, randomNum + 1, randomNum + 2); // MAKE an int array with 3 ints using the randomly generated number, that number incremented by 1, and that number incremented by 2 (example 3, 4, 5)
+   theStartup.setLocationCells(locations); // INVOKE the setLocationCells() method on the SimpleStartup instance (Give the startup its locations)
+   boolean isAlive = true;  // DECLARE a boolean variable representing the state of the game, named isAlive. SET it to true
    
-   while (numOfHits < 3) {      // WHILE the Startup is alive (isAlive == true) 
-     Scanner userInput = new Scanner(System.in) // Create scanner object
-     System.out.println("enter a number");
-     String guess = userInput.nextLiner();   // GET user input from the command line
-     // INCREMENT numOfGuesses variable
-     // // CHECK for Startup dead
+   while (isAlive) {      // WHILE the Startup is alive (isAlive == true) 
+     int guess = helper.getUserInput("enter a number");        // GET user input from the command line
+     String result = theStartup.checkYourself(guess);
+     numOfGuesses++;     // INCREMENT numOfGuesses variable
+     if (result.equals("kill")) {
+       isAlive = false;
+       System.out.pringln("You took " + numOfGuesses + " guesses"); // // CHECK for Startup dead
      // IF result is "kill"
      //    SET isAlive to false
      //    PRINT number of guesses
-     System.out.println(result);
    }
   } // End of method
 } // End of class
