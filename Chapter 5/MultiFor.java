f6a914017eaecd574a457b80502e69af71557fb5
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-12 14:52:20 +0000

    Add original snippets exercise

---
+++ Chapter 5/MultiFor.java
@@ -0,0 +1,15 @@
+// Put the snippets of code in the right order to produce the following output:
+// 0 4
+// 0 3
+// 1 4
+// 1 3
+// 3 4
+// 3 3
+
+i++;
+if (i == 1) {
+System.out.println(i + " " + j);
+class Multifor {
+for(int j = 4; j > 2; j-- {
+for(int i = 0; i < 4; i++){
+public static void main(String[] args) {
