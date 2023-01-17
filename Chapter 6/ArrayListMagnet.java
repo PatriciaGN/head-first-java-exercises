0a4d26a66c171db9a16d90428494baafa1378704
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-17 12:41:42 +0000

    Add original magnet exercise

---
+++ Chapter 6/ArrayListMagnet.java
@@ -0,0 +1,48 @@
+// Put the code snippets in the right order to achieve the output:
+// zero  one  two  three
+// zero  one  three  four
+// zero  one  three   four  4.2
+// zero  one  three  four  4.2
+
+a.remove(2);
+
+printList(a);
+
+printList(a);
+
+a.add(0, "zero");
+a.add(1, "one");
+
+public static void printList(ArrayList<String> list) {
+
+if (a.contains("two")) {
+  a.add("2.2");
+}
+
+public static void main (String[] args) {
+
+  System.out.print(element + " ");
+}
+System.out.println();
+
+if (a.contains("three)) {
+  a.add("four");
+}
+
+public class ArrayListMagnet {
+
+if (a.indexOf("four") != 4) {
+  a.add(4, "4.2");
+}
+
+import java.util.ArrayList;
+
+printList(a);
+
+ArrayList<String> a = new ArrayList<String>();
+
+for (String element : list) {
+
+a.add(3, "three");
+printList(a);
+
