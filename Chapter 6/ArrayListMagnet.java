15cc151dcef51fe72d0d9366512390a923a05ed8
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-17 14:43:47 +0000

    Complete magnets exercise and compile

--- Chapter 6/ArrayListMagnet.java
+++ Chapter 6/ArrayListMagnet.java
@@ -4,45 +4,46 @@
 // zero  one  three   four  4.2
 // zero  one  three  four  4.2
 
-a.remove(2);
-
-printList(a);
-
-printList(a);
-
-a.add(0, "zero");
-a.add(1, "one");
-
-public static void printList(ArrayList<String> list) {
-
-if (a.contains("two")) {
-  a.add("2.2");
-}
-
-public static void main (String[] args) {
-
-  System.out.print(element + " ");
-}
-System.out.println();
-
-if (a.contains("three)) {
-  a.add("four");
-}
-
-public class ArrayListMagnet {
-
-if (a.indexOf("four") != 4) {
-  a.add(4, "4.2");
-}
-
 import java.util.ArrayList;
 
-printList(a);
+public class ArrayListMagnet {
+  public static void printList(ArrayList<String> list) {
+    for (String element : list) {
+      System.out.print(element + " ");
+      }
+    System.out.println();
+  
+  }
+  
+  public static void main (String[] args) {
+    ArrayList<String> a = new ArrayList<String>();
+    a.add(0, "zero");
+    a.add(1, "one");
+    a.add(2, "two");
+    a.add(3, "three");
+    
+    printList(a);
+    
+    a.remove(2);
+    
+    if (a.contains("three")) {
+      a.add("four");
+    }
 
-ArrayList<String> a = new ArrayList<String>();
+    printList(a);
+    
+    if (a.contains("two")) {
+      a.add("2.2");
+    }
+    
+    if (a.indexOf("four") != 4) {
+      a.add(4, "4.2");
+    }
+    
+    printList(a);
+    printList(a);
+  }
+}
 
-for (String element : list) {
 
-a.add(3, "three");
-printList(a);
 
