2683b0c745ed8e9d5fc8bed4879a1794bb2abc48
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-01-23 17:24:48 +0000

    Original pool exercise to fill in with snippets

---
+++ Chapter 8/pool-exercise.java
@@ -0,0 +1,35 @@
+// Fill in the code with the given snippets to obtain the following output:
+// java _____________
+// 5 class Acts
+// 7 class Clowns
+// ____  Of76
+
+_____________ Nose {
+  __________________________
+}
+
+abstract class Picaso implements ________ {
+ ___________________________________
+   return 7;
+  }
+}
+
+class ___________ ______________________ _______________ {}
+
+class _______________ _______________ _______________ {
+   _____________________
+   return 5;
+  }
+}
+
+public ______________ _______________ extends Clowns {
+  public static void main(String[] args) {
+    _______________________________
+    i[0] = new ________________
+    i[1] = new _________________
+    i[2] = new _________________
+    for (int x = 0; x < 3; x++) {
+      System.out.prinln(_____________ + " " + ____________.getClass());
+    }
+  }
+}
