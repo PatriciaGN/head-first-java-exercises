40403cf3478e148cc051a52c33d17bbbbc614cf9
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-02-01 11:59:18 +0000

    Copy MockSongs code to Jukebox1 file and delete

--- Chapter 11/Jukebox1.java
+++ Chapter 11/Jukebox1.java
@@ -10,3 +10,16 @@ public class Jukebox1 {
     System.out.println(songList);
   }
 }
+
+class MockSongs {
+  public static List<String> getSongStrings() {
+    List<String> songs = new ArrayList<>();
+    songs.add("somersault");
+    songs.add("cassidy");
+    songs.add("$10");
+    songs.add("havana");
+    songs.add("Cassidy");
+    songs.add("50 ways");
+    return songs;
+  }
+}
