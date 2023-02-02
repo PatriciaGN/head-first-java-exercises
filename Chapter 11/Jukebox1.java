a77d05b5259355c6ed33df85f09337b544f90f67
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-02-02 12:38:51 +0000

    Change go method to take songsV2 list instead of string

--- Chapter 11/Jukebox1.java
+++ Chapter 11/Jukebox1.java
@@ -8,7 +8,7 @@ public class Jukebox1 {
   }
   
   public void go() {
-    List<String> songList = MockSongs.getSongStrings();
+    List<SongV2> songList = MockSongs.getSongsV2();
     System.out.println(songList);
     Collections.sort(songList);
     System.out.println(songList);
