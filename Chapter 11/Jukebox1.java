35df81844b7a6c26885fbc3660763462bfa10264
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-02-02 12:36:28 +0000

    Add method getSongsV2

--- Chapter 11/Jukebox1.java
+++ Chapter 11/Jukebox1.java
@@ -26,4 +26,16 @@ class MockSongs {
     songs.add("50 ways");
     return songs;
   }
+  
+  public static List<SongV2> getSongsV2() {
+    List<SongV2> songs = new ArrayList<>();
+    songs.add(new SongV2("somersault", "zero 7", 147));
+    songs.add(new SongV2("cassidy", "grateful dead", 158));
+    songs.add(new SonV2("$10", "hitchiker", 140));
+    songs.add(new SongV2("havana", "cabello", 105));
+    songs.add(new SongV2("Cassidy", "grateful dead", 158));
+    songs.add(new SongV2("50 ways", "simon", 102));
+    return songs;
+  }
+}
 }
