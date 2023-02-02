6aaa9203cde2d74a9afc4e7c6371c1f8b27f5f54
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-02-02 12:30:37 +0000

    Create SongV2 class

---
+++ Chapter 11/songV2.java
@@ -0,0 +1,23 @@
+class SOngV2 {
+  private String title;
+  private String artist;
+  private int bpm;
+  
+  SongV2(String title, String artlist, int bpm) {
+    this.title = title;
+    this.artist = artist;
+    this.bpm = bpm;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public String getArtist() {
+    return artist;
+  }
+  public int getBpm() {
+    return bpm;
+  }
+  public String toString() {
+    return title;                // This method overrides toString() in the Object class, so that instead
+  }                              of printing its identifier code, it will print the song title.
+}
