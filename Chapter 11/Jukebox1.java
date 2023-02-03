80ba36a967a1c90b087b47fc9cd4722bcf6c943f
PatriciaGN <pgarcia16@alu.ucam.edu>
2023-02-03 14:52:59 +0000

    Add ArtistCompare to compare songs by artist using Comparator

--- Chapter 11/Jukebox1.java
+++ Chapter 11/Jukebox1.java
@@ -11,7 +11,17 @@ public class Jukebox1 {
     List<SongV3> songList = MockSongs.getSongsV3();
     System.out.println(songList);
     Collections.sort(songList); // Now that we have a list of SongsV2, the sort method doesn't know what to sort it on (title, bpm...), so it doesn't compile. This is because Song is not a subtype of Comparable, so we cannot sort() the list of songs.
+    
+    ArtistCompare artistCompare = new ArtistCompare(); // Makes an instance of the Comparator class
+    songList.sort(artistCompare); // Invoke sort() on our list, passing it a reference to the new custom Comparator object.
+    
     System.out.println(songList);
+  }
+}
+
+class ArtistCompare implements Comparator<SongV3> { // We are lettin the String variables (for artist) do the actual
+  public int compare(SongV3 one, SongV3 two) {      // comparison, since Strings already know how to alphabetize themselves.
+    return one.getArtist().compareTo(two.getArtist());
   }
 }
 
